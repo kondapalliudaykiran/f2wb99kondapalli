ece5c4cfe28d49d5e5ecd78f9598173bfaec9f64
UDAY KIRAN KONDAPALLI <udaykiran4046@gmail.com>
2022-11-02 00:19:14 -0500

    pug

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # f2wb99kondapalli
+
+```
+class truck {
+        String truck_name;
+        var truck_type;
+        number truck_size;
+      }
+      
+```
--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var truckRouter = require('./routes/truck');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
+
+
 
 var app = express();
 
@@ -21,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/truck', truckRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
